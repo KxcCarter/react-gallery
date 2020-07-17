bf0e59ebca0b1adcb15ad00e92c45d1d4166229f
Kenneth Carter <kxccarter@gmail.com>
2020-07-17 15:33:37 -0500

    Creates GET route in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  getData() {
+    console.log(`in getData`);
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(`GET Error! ${err}`);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" alt="it's a goat" />
       </div>
     );
   }
