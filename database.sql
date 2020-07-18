ea48115f9c9af63f675bd62c457b52d3ff226755
Kenneth Carter <kxccarter@gmail.com>
2020-07-18 12:34:38 -0500

    Connects PUT route to database.

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE gallery
     "id" SERIAL PRIMARY KEY,
     "path" VARCHAR(200) NOT NULL,
     "description" VARCHAR(240) NOT NULL,
-    "likes" INT
+    "likes" INT DEFAULT 0
 );
 
 INSERT INTO gallery
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -7,19 +7,32 @@ const pool = require('../modules/pool');
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-  console.log(req.params);
-  const galleryId = req.params.id;
-  for (const galleryItem of galleryItems) {
-    if (galleryItem.id == galleryId) {
-      galleryItem.likes += 1;
-    }
-  }
-  res.sendStatus(200);
+  const query = `UPDATE gallery SET likes = likes + 1 WHERE id = $1;`;
+  const id = req.params.id;
+
+  pool
+    .query(query, [id])
+    .then((dbRes) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+
+  //   console.log(req.params);
+  //   const galleryId = req.params.id;
+  //   for (const galleryItem of galleryItems) {
+  //     if (galleryItem.id == galleryId) {
+  //       galleryItem.likes += 1;
+  //     }
+  //   }
+  //   res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-  const query = `SELECT * FROM gallery;`;
+  const query = `SELECT * FROM gallery ORDER BY id ASC;`;
 
   pool
     .query(query)
