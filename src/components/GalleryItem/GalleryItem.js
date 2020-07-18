bf503e4888d84eb3bb14a3fdeb756804c68deab7
Kenneth Carter <kxccarter@gmail.com>
2020-07-18 11:08:28 -0500

    Componentizes GalleryItem.

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import GalleryPhoto from '../GalleryPhoto/GalleryPhoto';
+import styles from '../GalleryItem/GalleryItem.module.css';
 
 class GalleryItem extends Component {
   render() {
-    const galleryItems = this.props.fullGallery.map((item, index) => {
-        return (
-          <div className={styles.galleryUnit} >
-            <GalleryPhoto img={item.path} description={item.description} />
-            <div className={styles.likeBox}>
-              <p>{item.likes}</p>
-              <i
-                class="fas fa-heart"
-                
-              ></i>
-            </div>
-          </div>
-        );
+    return (
+      <div className={styles.galleryUnit}>
+        <GalleryPhoto img={this.props.img} description={this.props.description} />
+        <div className={styles.likeBox}>
+          <p>{this.props.likes}</p>
+          <i class="fas fa-heart" onClick={this.props.clickAddLike}></i>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-import GalleryPhoto from '../GalleryPhoto/GalleryPhoto';
 import styles from '../GalleryList/GalleryList.module.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 class GalleryList extends Component {
   render() {
     const galleryItems = this.props.fullGallery.map((item, index) => {
       return (
-        <div className={styles.galleryUnit} key={item.id}>
-          <GalleryPhoto img={item.path} description={item.description} />
-          <div className={styles.likeBox}>
-            <p>{item.likes}</p>
-            <i
-              class="fas fa-heart"
-              onClick={(event) => {
-                this.props.addLike(item.id);
-              }}
-            ></i>
-          </div>
-        </div>
+        <GalleryItem
+          key={item.id}
+          img={item.path}
+          description={item.description}
+          likes={item.likes}
+          clickAddLike={(event) => {
+            this.props.addLike(item.id);
+          }}
+        />
       );
     });
 
